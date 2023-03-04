ceff94b31d4aacdc92b2c39d8eac9df2f37ee3e6
Kanav <73458114+Kanav129@users.noreply.github.com>
2023-03-04 18:52:01 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+	<script src="js/bootstrap/js/bootstrap.bundle.min.js"></script>
+	
+	<script src="js/bootstrap.bundle.js"></script>
+</head>
+
+<body>
+	<div class="hero-image">
+		<?php
+		require 'includes/header.php';
+
+		require 'includes/body.php';
+		?>
+	</div>
+	<?php
+	require 'includes/footer.php';
+	?>
+</body>	
+</html>
